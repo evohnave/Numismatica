d3e76bf62ec4c20dfe0f6b6f4a1182e89a8ebc15
Eric Vanhove <eric.vanhove@gmail.com>
2020-06-08 23:54:13 -0400

    Cleaned up code a little, working on part 2... adding values to df

--- TaulerYFau/tauler_y_fau.py
+++ TaulerYFau/tauler_y_fau.py
@@ -18,14 +18,8 @@ lot1img = 'img/thumbs/700/001/997/001-997-27_01.jpg'
 all_coins = 'https://www.tauleryfau.com/en/auctions/all-categories?_token=iNfGYfH8AQgptPuIWd9rXpWeJCxvQ6ROnW1p9VZY&s=2141&order=ref&total=603&reference=&description='
 all_no_token = 'https://www.tauleryfau.com/en/auctions/all-categories?s=2141&order=ref&total=603&reference=&description='
 
-
 lot2 = 'https://www.tauleryfau.com/en/lot/24062020-2141-2141/2-997-greek-coins'
 lot1 = 'https://www.tauleryfau.com/en/lot/24062020-2141-2141/1-997-greek-coins'
-#  Tauler y Fau has all their coins available at all_no_token
-#  Each lot is at https://www.tauleryfau.com/en/lot/<auction id>/lot#-###-category
-#    The lot can be read from the main page all_no_token
-
-#  After getting the list of lots, we can download the appropriate data
 
 r = requests.get(all_no_token)
 
@@ -37,27 +31,21 @@ lot_pattern = r'Lot\s+(\d{1,4})'
 lot_no_pattern = r'\/(\d{1,4})-.*$'
 category_pattern = r'\/\d{1,4}-\d{1,4}-(.*$)'
 columns = ['Lot', 'Link', 'Category']
+columns2 = ['images', 'description', 'currency', 'price']
 
-tyf = pd.DataFrame(columns=columns)
+tyf = pd.DataFrame(columns=columns+columns2)
 
 for item in soup.find_all('div', class_='lot-large-block-content'):
-    # # The items we want are in lot-large-block-content
-    # if item.has_attr('class'):
-    #     if 'lot-large-block-content' in item['class']:
-            # Get the link
     links = []
     for link in item.find_all('a'):
         links.append(link.get('href'))
-    # print(links)
 
     links = set(links)
-    # print(links)
     link = links.pop()
     if link:
         # Withdrawn lots may not have any links
         # Get lot number from link
         lot = re.search(lot_no_pattern, link)
-        # print(lot)
         if lot:
             lot = lot.groups()[0]
         else:
@@ -74,6 +62,9 @@ for item in soup.find_all('div', class_='lot-large-block-content'):
         
         tyf = tyf.append(row)
 
+tyf.set_index('Lot', inplace=True)
+
+
 img_pattern = r'https.*\.jpg'
 
 r = requests.get(lot1)
@@ -82,12 +73,8 @@ r.status_code
 
 soup = BeautifulSoup(r.text, 'html.parser')
 
-picture_links = set()
-
-# for item in soup.find_all('div', class_='item_content_img_single'):
-#     for link in item.find_all('img'):
-#         picture_links.add(link.get('src'))
-    
+columns2 = ['images', 'description', 'currency', 'price']
+tyf2 = pd.DataFrame(columns=columns2)
 
 for item in soup.find_all('script', attrs={"type": "application/ld+json"}):
 #    print(item)
@@ -95,18 +82,21 @@ for item in soup.find_all('script', attrs={"type": "application/ld+json"}):
 #    print(f"{dir(item)}")
 #    print ("\n\n\n")
 #    print(item.contents)
-    print("\n\n\n Item Contents as json?")
+    # print("\n\n\n Item Contents as json?")
     b = json.loads(item.contents[0])
-    print(b)
-    print ("\n\n\n")    
-    print(b['image'])
-    print ("\n\n\n")
-    print(b['description'])
-    print ("\n\n\n")
-    print(b['offers']['priceCurrency'])
-    print ("\n\n\n")
-    print(b['offers']['price'])
-    print ("\n\n\n")
+    # print(b)
+    # print ("\n\n\n")    
+    images = b['image']
+    # print ("\n\n\n")
+    description = b['description']
+    # print ("\n\n\n")
+    currency = b['offers']['priceCurrency']
+    # print ("\n\n\n")
+    price = b['offers']['price']
+    # print ("\n\n\n")
+    row = pd.DataFrame([[images, description, currency, price]],
+                       columns=columns2)
+    print(row)
     
 
 
