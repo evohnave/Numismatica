9da0cb3347b6c76a4666ce9ebd0b32ad07e0603d
evohnave <eric.vanhove@gmail.com>
2020-06-12 19:00:26 -0400

    Working for Tauler y Fau Auction 60
    Currently, it will look at all lots - you must supply the
    "all lots" url - and get the links to the lots, then look
    at each of the lots to get the information about the lot
    and the links to images, then it gets the images.  Also,
    it will save the dataframe in parquet format.
    
    Lots to be done, but we are on the way.

--- TaulerYFau/tauler_y_fau.py
+++ TaulerYFau/tauler_y_fau.py
@@ -11,7 +11,8 @@ import pandas as pd
 import re
 import json
 from time import sleep
-
+from pathlib import Path
+import shutil
 
 def fix_desc(bad_json):
     ''' Attempt to fix description '''
@@ -39,6 +40,18 @@ def fix_desc(bad_json):
         ret['offers']['price'] = None
     return ret
 
+def download(file_name, from_link, save_to_path):
+    ''' Downloads (image) file '''
+    r = requests.get(from_link, stream=True)
+
+    if r.status_code == 200:
+        r.raw.decode_content = True
+        with open(save_to_path.joinpath(file_name), 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+        return {"statusCode": 200, "path": str(save_to_path.joinpath(file_name))}
+    else:
+        return {"statusCode": r.status_code}
+
 
 
 base = 'https://www.tauleryfau.com'
@@ -61,7 +74,8 @@ lot_pattern = r'Lot\s+(\d{1,4})'
 lot_no_pattern = r'\/(\d{1,4})-.*$'
 category_pattern = r'\/\d{1,4}-\d{1,4}-(.*$)'
 columns = ['Lot', 'Link', 'Category']
-columns2 = ['images', 'description', 'currency', 'price']
+columns2 = ['images', 'description', 'currency',
+            'price', 'file_locs', 'downloads']
 
 tyf = pd.DataFrame(columns=columns+columns2)
 
@@ -89,7 +103,7 @@ for item in soup.find_all('div', class_='lot-large-block-content'):
             category = "Not listed"
         # add to df
         row = pd.DataFrame([[lot, link, category]], columns=columns)
-        
+
         tyf = tyf.append(row)
 
 tyf.set_index('Lot', inplace=True)
@@ -98,58 +112,45 @@ for lot in tyf.index:
     coin_url = base + tyf.Link.loc[lot]
     r = requests.get(coin_url)
     if r.status_code == 200:
-        soup = BeautifulSoup(r.text, 'html.parser')   
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-        for item in soup.find_all('script', 
+        for item in soup.find_all('script',
                                   attrs={"type": "application/ld+json"}):
             try:
                 b = json.loads(item.contents[0])
             except json.JSONDecodeError:
                 #tyf.description.loc[lot] = "Error - probably quotes inside JSON"
                 b = fix_desc(item.contents[0])
-                
+            # these are only the thumbnail images... next loop will get them
             tyf.images.loc[lot] = b['image']
             tyf.description.loc[lot] = b['description']
             tyf.currency.loc[lot] = b['offers']['priceCurrency']
             tyf.price.loc[lot] = b['offers']['price']
+        images = []
+        imgs = soup.find_all('img', class_='img-responsive')
+        for image in tyf.images.loc[lot]:
+            name = image.split('/')[-1]
+            for src in imgs:
+                try:
+                    if src['src'].endswith(name):
+                        images.append(src['src'])
+                except KeyError:
+                    pass
+        tyf.images.loc[lot] = images
+        # download the images
+        file_locs = []
+        for image in tyf.images.loc[lot]:
+            file_name = image.split('/')[-1]
+            from_link = image
+            save_to_path = Path(r'C:/Users/Cire/Downloads/TaulerYFau60/images/')
+            file_locs.append(download(file_name, from_link, save_to_path))
+        tyf['file_locs'].loc[lot] = file_locs
 
     sleep(3)
-    
 
+# Save current efforts
+save_df = Path(r'C:/Users/Cire/Downloads/TaulerYFau60/Auction60.gzip')
+tyf.to_parquet(save_df, compression='gzip')
 
-
-            
-    
-
-        
-                    
-            
-            
-            
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Saving images to disk
 
